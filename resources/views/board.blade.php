b4bb60470ca2f5e8a56f7fdccceb5900e5581843
AKASH <mdrakash32@gmail.com>
2022-10-05 17:45:31 +0600

    html design

---
+++ resources/views/board.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kanban::Board</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-lg-6">
+                <form class="mt-5">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Write Your Task ...">
+                        <button class="mr-3 btn btn-outline-secondary" type="button" id="addtask">Add</button>
+                    </div>
+                </form>
+            </div>
+            
+
+        </div>
+    </div>
+
+    <script src="{{ asset('board.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
